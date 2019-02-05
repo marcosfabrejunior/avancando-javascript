ab0e25e8d9bce03de7c8b22fe04808135cef8de1
marcosfabrejr <marcosfabrejunior@hotmail.com>
2019-02-05 08:23:00 -0200

    atividade 3 - aula 4 - Concluído

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -12,8 +12,7 @@ class NegociacaoController{
     adiciona(event){
         event.preventDefault();
 
-        let data = new Date(...this._inputData.value.split('-')
-        .map((item, indice) => item - indice % 2));
+        let data = DateHelper.textoParaData(this._inputData.value);
 
         let negociacao = new Negociacao(
             data, 
@@ -21,6 +20,7 @@ class NegociacaoController{
             this._inputValor.value
         );
 
-        console.log(negociacao);
+       
+        
     }
 }
