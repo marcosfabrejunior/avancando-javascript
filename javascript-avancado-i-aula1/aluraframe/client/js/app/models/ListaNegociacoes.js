2f2021c94a716ccc7d8d48494b0c176d7cc43ab4
marcosfabrejr <marcosfabrejunior@hotmail.com>
2019-02-13 08:21:12 -0200

    Refazer aula 2 completa

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,16 +1,14 @@
 class ListaNegociacoes {
     
-    constructor(armadilha) {
+    constructor() {
         
         this._negociacoes = [];
-        this._armadilha = armadilha;
         this._contexto = contexto;
     }
     
     adiciona(negociacao) {
         
         this._negociacoes.push(negociacao);
-        this._armadilha(this);
         // Reflect.apply(this._armadilha, this._contexto,[this]);
     }
     
@@ -21,7 +19,6 @@ class ListaNegociacoes {
 
     esvazia(){
         this._negociacoes = [];
-        this._armadilha(this);
         // Reflect.apply(this._armadilha, this._contexto,[this]);
     }
 }
